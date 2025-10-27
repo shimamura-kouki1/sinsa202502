75ed16bfabd4088a57d92ca9040440b87cbefca7
shimamura <shimakou779@gmail.com>
2025-10-27 20:08:35 +0900

    [update]斜め移動が1.4倍になっていたのを正規化して、どの角度でも等速に動くようにした

--- Assets/Script/Player/Player1.cs
+++ Assets/Script/Player/Player1.cs
@@ -51,9 +51,8 @@ public class Player1 : MonoBehaviour
         //======== ˆÚ“® =========
         if (_playerInput.actions["Move"].IsPressed())
         {
-            Debug.Log("aa");
             _horizontal = _move.ReadValue<Vector2>();
-            _playerPostion = new Vector3(_horizontal.x, 0, _horizontal.y);
+            _playerPostion = new Vector3(_horizontal.x, 0, _horizontal.y).normalized;
             _rb.MovePosition(_tr.position + _playerPostion * _moveSpeed * Time.fixedDeltaTime);
         }
     }
