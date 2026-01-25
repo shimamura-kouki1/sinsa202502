5ec464b3c332278d1bae5b9df2b1f66add79a2ff
shimamura <shimakou779@gmail.com>
2026-01-25 23:30:47 +0900

    [add]Healthのクラスを作成、HPを所有するオブジェクトにつける。受けたダメージと死亡の通知を出す

---
+++ Assets/Script/Enemy/Enemy.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IDamageable
+{
+    [SerializeField] private EnemyDate _enemyDate;
+
+    private float _hp;
+
+    private void Start()
+    {
+        _hp = _enemyDate._maxHp;
+    }
+    public void TakeDamage(float damage)
+    {
+        _hp -= damage;
+        Debug.Log(_hp);
+        if (_hp <= 0f)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        transform.position =  _enemyDate._despoilAnchor.position;
+        Debug.Log("Dei");
+    }
+}
--- Assets/Script/Enemy/Health.cs
+++ Assets/Script/Enemy/Health.cs
@@ -1,27 +1,35 @@
+using System;
 using UnityEngine;
-
-public class Health : MonoBehaviour, IDamageable
+/// <summary>
+/// HPの管理、ダメージと死亡の通知をする
+/// </summary>
+public class Health : MonoBehaviour,IDamageable
 {
-    [SerializeField] private EnemyDate _enemyDate;
+    [SerializeField] private float _maxHp = 100f;
 
-    private float _hp;
+    private float _currentHp;
 
-    private void Start()
+    public event Action OnDeath;//死んだ通知
+    public event Action<float> OnDamage;//ダメージ量の通知　UI用
+
+    private void Awake()
     {
-        _hp = _enemyDate._maxHp;
+        _currentHp = _maxHp;
     }
-    public void TakeDamage(float damage)
+
+    public void TakeDamage(float damege)
     {
-        _hp -= damage;
-        Debug.Log(_hp);
-        if (_hp <= 0f)
+        if (_currentHp < 0f) return;//0以下ならリターン
+
+        _currentHp -= damege;
+        _currentHp = Math.Max(_currentHp, 0f);//
+
+        OnDamage?.Invoke(damege);//ダメージの通知
+
+        if(_currentHp <= 0f)
         {
-            Die();
+            OnDeath?.Invoke();//HPが0になったら通知
         }
-    }
-    private void Die()
-    {
-        transform.position =  _enemyDate._despoilAnchor.position;
-        Debug.Log("Dei");
+
     }
 }
