6a3670a034461dcdafa16619ea7830742933a726
shimamura <shimakou779@gmail.com>
2026-01-26 11:54:30 +0900

    [update]enemyAI基板完成　ターゲット追跡と攻撃を作成

--- Assets/Script/Enemy/EnemyAi.cs
+++ Assets/Script/Enemy/EnemyAi.cs
@@ -1,3 +1,5 @@
+using System.Data.Common;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class EnemyAi : MonoBehaviour
@@ -8,6 +10,15 @@ public class EnemyAi : MonoBehaviour
     private float _attakcDamage;
 
     private Transform _playerPos;
+
+    /// <summary>
+    /// プレイヤーを注入される
+    /// </summary>
+    /// <param name="player"></param>
+    public void TargetSet(Transform player)
+    {
+        _playerPos = player;
+    }
     void Start()
     {
         _moveSpeed = _enemyDate._moveSpeed;
@@ -16,6 +27,34 @@ public class EnemyAi : MonoBehaviour
     }
     void Update()
     {
-        
+        float distance = Vector3.Distance(transform.position, _playerPos.position);
+
+        if (distance > _attackRenge)
+        {
+            MoveToTarget();
+        }
+        else
+        {
+            Attack();
+        }
+
     }
-}
+    /// <summary>
+    /// ターゲットへの移動
+    /// </summary>
+    private void MoveToTarget()
+    {
+        Vector3 direction = (transform.position - _playerPos.position).normalized;
+        transform.position += direction * _moveSpeed * Time.deltaTime;
+    }
+    /// <summary>
+    /// 攻撃
+    /// </summary>
+    private void Attack()
+    {
+        if(_playerPos.TryGetComponent<IDamageable>(out var damageable))
+        {
+            damageable.TakeDamage(_attakcDamage);
+        }
+    }
+    }
